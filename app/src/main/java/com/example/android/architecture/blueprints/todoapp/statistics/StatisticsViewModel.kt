f9f1cee9c8f0c99575e02b7f2375fcdc7641ace5
Kulbaka Nataly <kulbacka.natalya@gmail.com>
2021-12-06 15:25:31 +0200

    add viewmodel test

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsViewModel.kt
@@ -24,14 +24,15 @@ import com.example.android.architecture.blueprints.todoapp.data.Result.Error
 import com.example.android.architecture.blueprints.todoapp.data.Result.Success
 import com.example.android.architecture.blueprints.todoapp.data.Task
 import com.example.android.architecture.blueprints.todoapp.data.source.DefaultTasksRepository
+import com.example.android.architecture.blueprints.todoapp.data.source.TasksRepository
 import kotlinx.coroutines.launch
 
 /**
  * ViewModel for the statistics screen.
  */
-class StatisticsViewModel(application: Application) : AndroidViewModel(application) {
-
-    private val tasksRepository = (application as TodoApplication).taskRepository
+class StatisticsViewModel(
+        private val tasksRepository: TasksRepository
+) : ViewModel() {
 
     private val tasks: LiveData<Result<List<Task>>> = tasksRepository.observeTasks()
     private val _dataLoading = MutableLiveData<Boolean>(false)
@@ -57,4 +58,11 @@ class StatisticsViewModel(application: Application) : AndroidViewModel(applicati
                 _dataLoading.value = false
             }
     }
+}
+@Suppress("UNCHECKED_CAST")
+class StatisticsViewModelFactory (
+        private val tasksRepository: TasksRepository
+) : ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>) =
+            (StatisticsViewModel(tasksRepository) as T)
 }
